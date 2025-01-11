db68d49bf69c09417fb5bd682cb84533f6845237
Henrique Dias <hdias8969@gmail.com>
2025-01-11 10:57:30 -0300

    separation of functionality creates finished list items

--- main.js
+++ main.js
@@ -1,50 +1,12 @@
-const inputItem = document.getElementById('input-item')
+import { criarItemDaLista } from "./srcipts/criarItemDaLista.js"
+
 const botaoAdicionar = document.getElementById('adicionar-item')
 const listaDeCompras = document.getElementById('lista-de-compras')
-let contador = 0
 
 botaoAdicionar.addEventListener('click', (evento) => {
     evento.preventDefault()
-    if (inputItem.value === '') {
-        alert('Por favor, insira um item')
-        return
-    }
-
-    const itemDaLista = document.createElement('li') //* CRIA UM ELEMENTO LI
-    const containerItemDaLista = document.createElement('div')
-    containerItemDaLista.classList.add('lista-item-container')
-    const inputCheckBox = document.createElement('input') //* CRIA UM ELEMENTO INPUT
-    inputCheckBox.type = 'checkbox' //* TRANSFORMA O TIPO DO INPUT EM CHECKBOX
-    inputCheckBox.id = 'checkbox-' + contador++
-    const nomeItem = document.createElement('p')
-    nomeItem.innerText = inputItem.value //* PEGA O TEXTO QUE FOI DIGITADO 
-
-    inputCheckBox.addEventListener('click', () => {
-        if (inputCheckBox.checked) {
-            nomeItem.style.textDecoration = 'line-through'
-        } else {
-            nomeItem.style.textDecoration = 'none'
-        }
-    })
-
-    containerItemDaLista.appendChild(inputCheckBox)
-    containerItemDaLista.appendChild(nomeItem)
-
-    itemDaLista.appendChild(containerItemDaLista)
-
-    const diaDaSemana = new Date().toLocaleDateString('pt-BR', { weekday: 'long' })
-    const data = new Date().toLocaleDateString('pt-BR')
-    const hora = new Date().toLocaleTimeString('pt-BR', { hour: 'numeric', minute: 'numeric' })
-
-    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
-    const itemData = document.createElement('p')
-    itemData.innerText = dataCompleta // Corrigido para atribuir ao itemData
-    itemData.classList.add('texto-data')
-
-    itemDaLista.appendChild(itemData) // Corrigido para anexar itemData ao itemDaLista
-
+    const itemDaLista = criarItemDaLista()
     listaDeCompras.appendChild(itemDaLista)
-
     verificarListaVazia()
 })
 
---
+++ srcipts/criarItemDaLista.js
@@ -0,0 +1,44 @@
+const inputItem = document.getElementById('input-item')
+let contador = 0
+export function criarItemDaLista() {
+    
+    if (inputItem.value === '') {
+        alert('Por favor, insira um item')
+        return
+    }
+
+    const itemDaLista = document.createElement('li') //* CRIA UM ELEMENTO LI
+    const containerItemDaLista = document.createElement('div')
+    containerItemDaLista.classList.add('lista-item-container')
+    const inputCheckBox = document.createElement('input') //* CRIA UM ELEMENTO INPUT
+    inputCheckBox.type = 'checkbox' //* TRANSFORMA O TIPO DO INPUT EM CHECKBOX
+    inputCheckBox.id = 'checkbox-' + contador++
+    const nomeItem = document.createElement('p')
+    nomeItem.innerText = inputItem.value //* PEGA O TEXTO QUE FOI DIGITADO 
+
+    inputCheckBox.addEventListener('click', () => {
+        if (inputCheckBox.checked) {
+            nomeItem.style.textDecoration = 'line-through'
+        } else {
+            nomeItem.style.textDecoration = 'none'
+        }
+    })
+
+    containerItemDaLista.appendChild(inputCheckBox)
+    containerItemDaLista.appendChild(nomeItem)
+
+    itemDaLista.appendChild(containerItemDaLista)
+
+    const diaDaSemana = new Date().toLocaleDateString('pt-BR', { weekday: 'long' })
+    const data = new Date().toLocaleDateString('pt-BR')
+    const hora = new Date().toLocaleTimeString('pt-BR', { hour: 'numeric', minute: 'numeric' })
+
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
+    const itemData = document.createElement('p')
+    itemData.innerText = dataCompleta // Corrigido para atribuir ao itemData
+    itemData.classList.add('texto-data')
+
+    itemDaLista.appendChild(itemData) // Corrigido para anexar itemData ao itemDaLista
+
+    return itemDaLista
+}
